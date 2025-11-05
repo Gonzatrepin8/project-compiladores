ba6084cffd94ffc78ad70e21522df36dcb55672a
florh1968 <florenciah1968@gmail.com>
2025-11-04 23:13:08 -0300

    Add binary operator and optimization flags enums to select what to use
    Co-authored-by: eg-v <eg-v@users.noreply.github.com>

--- src/opt/opt.h
+++ src/opt/opt.h
@@ -6,7 +6,29 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+typedef enum {
+    BINOP_SUM,
+    BINOP_SUB,
+    BINOP_MULT,
+    BINOP_DIV,
+    BINOP_MOD,
+    BINOP_LESS,
+    BINOP_GREATER,
+    BINOP_EQ,
+    BINOP_OR,
+    BINOP_AND
+} binops;
+
+typedef enum {
+    OPT_DEAD_CODE,
+    OPT_CONSTANT_FOLDING,
+    OPT_SHORT_CIRCUIT_EVALUATION,
+    OPT_PEEPHOLE
+} opt_flags;
+
 void dead_code(AST *n);
 void const_prop(AST *n);
+int optimize_int_operators(int a, int b, binops op);
+bool optimize_bool_operators(int a, int b, binops op);
 
 #endif
